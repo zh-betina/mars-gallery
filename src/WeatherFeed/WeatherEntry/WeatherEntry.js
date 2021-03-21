c75c26c840dbde6340cedd8f9ca6fd3cebae0733
betina <zh.betina@gmail.com>
2021-03-21 14:14:14 +0100

    Add basic weather display

--- src/WeatherFeed/WeatherEntry/WeatherEntry.js
+++ src/WeatherFeed/WeatherEntry/WeatherEntry.js
@@ -1,52 +1,30 @@
-import Icon from '@mdi/react';
-import { mdiThermometerLines } from '@mdi/js';
-import { mdiSnowflake } from '@mdi/js';
-import { mdiFlower } from '@mdi/js';
+//import Icon from 'mdi-react';
+
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
-import { mdiLeafMaple } from '@mdi/js';
-import { mdiWeatherWindy } from '@mdi/js';
-import { mdiArrowCollapse } from '@mdi/js';
 
 import "./WeatherEntry.css";
 
 const WeatherEntry = props => {
     
-    const SeasonIcon = () => {
-        switch (props.season) {
-            case "winter":
-                <Icon path={mdiSnowflake} />
-                break;
-            case "spring":
-                <Icon path={mdiFlower} />
-                break;
-            case "summer":
-                <WbSunnyIcon />
-                break;
-            case "fall":
-                <Icon path={mdiLeafMaple} />
-                break;
-            default:
-                break;
-        }
-    }
+    const dateFormat = props.First_UTC.substr(0, 10);
     return (
         <div className="WeatherEntry-wrapper">
             {props.children}
             <div className="row">
                 <p className="WeatherEntry-date">Sol {props.sol}</p>
-                <p className="WeatherEntry-date">{props.First_UTC}</p>
+                <p className="WeatherEntry-date">{dateFormat}</p>
             </div>
             <div className="row">
-                <Icon path={mdiThermometerLines} /><p>Av. atmospheric temp.: {props.AT}°F</p>
+                <p>Av. atmospheric temp.: {props.AT}°F</p>
             </div>
             <div className="row">
-                <Icon path={mdiWeatherWindy} /><p>Av. horizontal wind speed: {props.HWS} m/s</p>
+                <p>Av. horizontal wind speed: {props.HWS} m/s</p>
             </div>
             <div className="row">
-                <Icon path={mdiArrowCollapse} /><p>Av. pressure: {props.PRE} Pa</p>
+                <p>Av. pressure: {props.PRE} Pa</p>
             </div>
             <div className="row">
-                <p>Season: {props.Season}</p><SeasonIcon />
+                <p>Season: {props.Season}</p>
             </div>
         </div>
     )
--- src/WeatherFeed/WeatherFeed.js
+++ src/WeatherFeed/WeatherFeed.js
@@ -8,6 +8,8 @@ import WeatherEntry from "./WeatherEntry/WeatherEntry";
 
 import fetchData from "../services/fetchData";
 
+import "./WeatherFeed.css";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
@@ -15,7 +17,8 @@ const useStyles = makeStyles((theme) => ({
     width: "16vw",
     top: "0",
     right: "0",
-    zIndex: "10"
+    zIndex: "10",
+    paddingTop: "15rem"
   },
 }));
 
@@ -36,33 +39,35 @@ const WeatherFeed = () => {
     fetchData(data, url, method)
       .then((data) => {
         console.log(data);
+
         for (const key in data.sol_keys) {
           loadedEntries.push({
             sol: data.sol_keys[key]
           })
         }
+
         loadedEntries.forEach((el, idx) => {
-          keys.forEach(keysEl=>{
-            if(Object.keys(data[el.sol]).includes(keysEl)){
+          keys.forEach(keysEl => {
+            if (Object.keys(data[el.sol]).includes(keysEl)) {
               loadedEntries[idx][keysEl] = data[el.sol][keysEl];
-              if(keysEl == "AT" || keysEl == "PRE" || keysEl == "HWS"){
+              if (keysEl == "AT" || keysEl == "PRE" || keysEl == "HWS") {
                 loadedEntries[idx][keysEl] = data[el.sol][keysEl].av;
               }
             }
-          });
+          })
         });
         console.log(loadedEntries);
         setWeatherEntries(loadedEntries);
       })
       .then(() => setLoader(false));
   }, []);
-
   console.log(weatherEntries);
   return (
     <Paper className={classes.root} elevation={3}>
+      <h3 className="Title-weather">Weather on Mars</h3>
       {
-        loader ? <CircularProgress /> : loadedEntries.map((el, idx)=>{
-          <WeatherEntry {...el} key={idx}/>
+        loader ? <CircularProgress /> : weatherEntries.map((items, idx) => {
+          return <WeatherEntry {...items} key={idx} />
         })
       }
     </Paper>
